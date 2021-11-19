6b57af731afd9ab9568c6372f0ffec766c243436
Tim Peter Edstrøm <tim.peter.edstrom@gmail.com>
2021-11-19 09:26:44 +0100

    Fix output and stop window from closing once done

--- Replace-StringInFiletype.ps1
+++ Replace-StringInFiletype.ps1
@@ -2,10 +2,16 @@
 .SYNOPSIS
     PowerShell script that replaces (server)nameA with (server)nameB
     Set a root folder and the script will run through all files of the configured type
-
 .DESCRIPTION
-    Long description
+    Script loops through files in the provided directory and subdirectories
+    It looks for files with a certain filename extension
+    If 'stringA' is found in a file it will replace it with 'stringB'
+    Finally, it will output the result
+    Script window will not automatically close to allow the user to view the summary
 .EXAMPLE
+    If script doesn't run, run the command below first (only required to do once)
+    PS C:\> Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
     PS C:\> Set-StringInFiletype.ps1
     Executes the script (remember, variables must be configured prior to execution)
 .INPUTS
@@ -18,6 +24,10 @@
     Date of origin 17.11.21
     Idea by Tim Peter Edstrøm
 
+    Modified 19.11.21 - Tim Peter Edstrøm
+    Reorganized output in a summary once the script is done processing files and folders
+    Added a Read-Host | Out-Null to stop script window from closing once run is complete
+
     Feel free to re-use and modify to suit your requirements.
 #>
 
@@ -26,12 +36,14 @@ $folder = ""
 $fileExtension = ""
 $stringA = ""
 $stringB = ""
+[System.Collections.ArrayList] $emptyDirs = @()
+[System.Collections.ArrayList] $filesModified = @()
 
 # Ask for folder if the $folder variable is not set
 if ([System.String]::IsNullOrEmpty($folder) ) {
     $folder = Read-Host -Prompt "Enter source folder"
-    Write-Host "Is this folder correct: $($folder) ?" -ForegroundColor DarkMagenta
-    Write-Host "Press ENTER to continue or CTRL+C to start script again..."
+    Write-Host "Is this folder correct: $($folder) ?" -ForegroundColor Yellow
+    Write-Host "Press ENTER to continue or CTRL+C to abort execution and start script again..."
     Read-Host | Out-Null
 }
 
@@ -39,7 +51,7 @@ if ([System.String]::IsNullOrEmpty($folder) ) {
 try {
     # Test for existence of path provided and abort if the folder is missing on inaccessible
     if (-not (Test-Path $folder)) {
-        Write-Host "Can't find or access directory $($folder), please try again! Aborting..." -ForegroundColor Red
+        Write-Host "Can't find or access directory '$($folder)', please try again! Aborting..." -ForegroundColor Red
         return
     }else {
         # Create an (re-sizeable) array list of directories
@@ -63,11 +75,11 @@ try {
             foreach ($file in $files | Where-Object { (Get-Content -Path ($directory + $_)) -match $stringA } ) {
                 $content = (Get-Content -Path ($directory + $file)).Replace($stringA, $stringB)
                 Set-Content -Value $content -Path ($directory + $file)
-                Write-Host "Successfully replaced '$stringA' with '$stringB' in file $($directory + $file)" -ForegroundColor Green
-                $success++
+                $filesModified.Add($($directory + $file)) | Out-Null
+                $successNumber++
             }
         }else {
-            Write-Host "No files were found in directory $($directory)" -ForegroundColor Yellow
+            $emptyDirs.Add($directory) | Out-Null
         }
     }
 }
@@ -77,11 +89,26 @@ catch {
     return
 }
 
-if (-not [System.String]::IsNullOrEmpty($success)) { 
-    Write-Host "`nA total of $($success) entries were replaced during this run.`nExceution completed successfully!" -ForegroundColor Green
+#region Issue a summary
+#
+if (-not [System.String]::IsNullOrEmpty($successNumber)) { 
+    # Output all directories run through without files that were modified (yellow)
+    foreach ($dir in $emptyDirs) {
+        Write-Host "No files found in directory $dir" -ForegroundColor Yellow
+    }
+    # Output all files, with full filepath, that were modified (green)
+    foreach ($modified in $filesModified) {
+        Write-Host "Successfully replaced '$stringA' with '$stringB' in file $modified" -ForegroundColor Green
+    }
+    
+    Write-Host "`nA total of $($successNumber) entries were replaced during this run." -ForegroundColor Green
 }else {
     Write-Host "No changes were made..." -ForegroundColor Yellow
 }
 
+Write-Host "`nScript execution complete. Press ENTER or CTRL+C to finish." -ForegroundColor Cyan
+Read-Host | Out-Null
+#endregion
+
 # Cleanup
-$folder, $fileExtension, $stringA, $stringB, $directories, $directory, $files, $file, $success = $null
+$folder, $fileExtension, $stringA, $stringB, $directories, $directory, $files, $file, $successNumber, $emptyDirs, $filesModified = $null
